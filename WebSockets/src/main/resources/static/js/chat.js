f7da5e8fc713dd4221490d4095ebe04f76e1b07a
Aina <ainaard@mail.ru>
2019-12-12 23:37:30 +0300

    docker

--- WebSockets/src/main/resources/static/js/chat.js
+++ WebSockets/src/main/resources/static/js/chat.js
@@ -9,10 +9,6 @@ var connectingElement = document.querySelector('.connecting');
 var stompClient = null;
 var username = null;
 var password = null;
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
 
 function connect(event) {
     username = document.querySelector('#name').value.trim();
@@ -55,16 +51,6 @@ function sendMessage(event) {
             type: 'CHAT'
         };
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-
-        // stompClient.send({
-        //     url: '/app/chat.sendMessage',
-        //     data: {
-        //         'content': chatMessage
-        //     },
-        // headers: {
-        //     "AUTH": window.localStorage.getItem("AUTH")
-        // }
-        // });
         messageInput.value = '';
     }
     event.preventDefault();
@@ -83,9 +69,6 @@ function onMessageReceived(payload) {
         messageElement.classList.add('chat-message');
         var avatarElement = document.createElement('i');
         console.log(message);
-        // var avatarText = document.createTextNode(message.sender[0]);
-        // avatarElement.appendChild(avatarText);
-        // avatarElement.style['background-color'] = getAvatarColor(message.sender);
         messageElement.appendChild(avatarElement);
         var usernameElement = document.createElement('span');
         var usernameText = document.createTextNode(message.sender);
@@ -98,15 +81,6 @@ function onMessageReceived(payload) {
     messageElement.appendChild(textElement);
     messageArea.appendChild(messageElement);
     messageArea.scrollTop = messageArea.scrollHeight;
-}
-
-function getAvatarColor(messageSender) {
-    var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-    var index = Math.abs(hash % colors.length);
-    return colors[index];
 }
 
 usernameForm.addEventListener('submit', connect, true);
